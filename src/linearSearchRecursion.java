49bf72011df65ceb03b51626dc04cae178c8d550
Aqua Marine <aquamarinewater2023@gmail.com>
2026-03-26 14:20:53 +0530

    corrected linear search by Recursion

--- src/linearSearchRecursion.java
+++ src/linearSearchRecursion.java
@@ -1,10 +1,13 @@
 public class linearSearchRecursion {
     public static void main(String[] args) {
     int[] arr = {3,2,1,188,9};
-    int idx = find(arr,188,0);
+    int idx = find(arr,9,0);
         System.out.println(idx);
     }
     static public int find(int[] arr, int target, int i){
+        if(i == arr.length){
+            return -1;
+        }
         if(target == arr[i]){
             return i;
         }
