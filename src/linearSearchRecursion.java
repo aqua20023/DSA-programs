dbf42f29e81b771a43e1959ee731dcbf62d2c230
Aqua Marine <aquamarinewater2023@gmail.com>
2026-03-26 14:33:55 +0530

    linear search by Recursion and finding position

--- src/linearSearchRecursion.java
+++ src/linearSearchRecursion.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class linearSearchRecursion {
     public static void main(String[] args) {
     int[] arr = {3,2,1,188,9};
+    int[] num = {1,1,2,2,4,6,4,7,3,61,7,3,2,7,3,7,0,4,2,9,27,1,1,1,1,1,1,1,11,1};
     int idx = find(arr,9,0);
+    findingPos(num,1,0);
         System.out.println(idx);
+        System.out.println(list);
+
     }
     static public int find(int[] arr, int target, int i){
         if(i == arr.length){
@@ -12,6 +18,16 @@ public class linearSearchRecursion {
             return i;
         }
         return find(arr, target, ++i);
+    }
+     static ArrayList<Integer> list = new ArrayList<>();
+    static public void findingPos(int[] arr,int target,int i ){
+        if(i == arr.length){
+            return;
+        }
+        if(arr[i] == target){
+            list.add(i);
+        }
+        findingPos(arr, target, ++i);
     }
 
 }
